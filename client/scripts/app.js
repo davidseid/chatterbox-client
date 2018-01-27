ab41bfcf12270854d3eeba634f753d38b828ea53
davidseid <davidseidenberg@gmail.com>
2018-01-26 17:30:56 -0800

    Finish Chatroom Behavior basic functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,7 +13,7 @@ var app = {
 app.init = function() {
 
 };
-app.send = function(message) {
+app.send = function send (message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
@@ -30,6 +30,38 @@ app.send = function(message) {
   });
 };
 
+app.fetch = function fetch () {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: undefined,
+    type: 'GET',
+    data: 'messages',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function clearMessages () {
+  $('#chats').empty();
+};
+
+app.renderMessage = function renderMessage (message) {
+  var $msg = $('<div class="message"></div>');
+  $msg.text(message);
+  $('#chats').append($msg);
+};
+
+app.renderRoom = function renderRoom (roomName) {
+  var $room = $('<div class="room"></div>');
+  $room.text(roomName);
+  $('#roomSelect').append($room);
+};
 
 
 
