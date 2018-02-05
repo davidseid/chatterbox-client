49b31ff3dd325ce09af557afd7c1d185e1caf1a3
David Seidenberg <davidseidenberg@gmail.com>
2018-02-05 11:51:21 -0800

    Cleaning git status for upstream pull of solution code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,42 @@
-// YOUR CODE HERE:
+
+var message = {
+  username: 'david',
+  text: 'hello world',
+  roomname: 'sfm8'
+};
+
+$('button').on('click', function() {
+  $('.input').append('<p>HI</p>');
+});
+
+
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });// YOUR CODE HERE:
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+//   type: 'GET',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });// YOUR CODE HERE:
